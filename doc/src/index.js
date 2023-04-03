cf507b21a17817fee1f5e8491646fbcf94b18719
Zaw Lin Tun <zawlintun@utycc.edu.mm>
2023-04-03 20:34:36 +0630

    change static yml

--- doc/src/index.js
+++ doc/src/index.js
@@ -8,8 +8,10 @@ const app = createApp(Main)
 
 for (let component of Object.keys(import.meta.glob('./components/**/*.vue'))) {
     let componentName = [...component.matchAll(/([^\/]*).vue/g)][0][1];
-    let componentPath = component.replace('./components/', '').replace('.vue', '');
-    app.component(componentName, defineAsyncComponent(() => import('./components/' + componentPath + '.vue')))
+    let componentPath = component
+        // .replace('./components/', '')
+        .replace('.vue', '');
+    app.component(componentName, defineAsyncComponent(() => import( componentPath + '.vue')))
 }
 
 app.mount('#app')
